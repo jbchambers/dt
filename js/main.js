deff46dc659107d49a0d1b103902add3db98dd15
Jeff Chambers <jeffreybryanchambers@gmail.com>
2017-06-30 12:20:47 -0700

    Styled the 'Surround Yourself with Great People' section, used BX Slider again, used repeating content to save time

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
+    // LOVE WHAT YOU DO
     $('.love-carousel').bxSlider({
         minSlides: 3,
         maxSlides: 3,
         slideMargin: 10,
         ticker: true,
         speed: 15000
+    });
+    // GREAT PEOPLE
+    $('.team-carousel').bxSlider({
+        minSlides: 1,
+        maxSlides: 1,
+        slideMargin: 0,
+        easing: 'ease-in-out'
     });
 });
