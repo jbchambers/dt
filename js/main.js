95fc6ae5cc3a35178991a9161511e4eb4c1c7645
Jeff Chambers <jeffreybryanchambers@gmail.com>
2017-06-30 11:55:25 -0700

    Added bxslider.js, Implemented on 'Love What You Do' carousel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+    $('.love-carousel').bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideMargin: 10,
+        ticker: true,
+        speed: 15000
+    });
+});
